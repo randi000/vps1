1ae9d7f9c402ac10b43b73954047b49794bb62f0
randi000 <d.emon.waifu414@gmail.com>
2024-12-01 09:24:01 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,7 +3,7 @@ Get-LocalUser -Name "Administrator" | Enable-LocalUser
 Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
 tar xf ngrok.zip
 Copy ngrok.exe C:\Windows\System32
-cmd /c echo ./ngrok.exe authtoken "231cRs2QRMrxAhirwrDfnTbppGe_3MLvtFa176NosFWW9piBm" >a.ps1
+cmd /c echo ./ngrok.exe authtoken "1msFOAezxYgaeXrAcoUK6ssfew4_4LT6p5JT9DhaQ48amoVQW" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
 .\a.ps1
